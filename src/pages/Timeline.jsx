856ac9a4f059ccfe736c26c391e0ff45d0f168d3
kumarshantanu01 <shantanuk280@gmail.com>
2021-08-09 20:36:01 +0530

    timeline updated 02

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -16,41 +16,25 @@ const Timeline = () => {
                     <div className="card">
                         <div className="timeline-date" >
                         <p className="date" > 10th October 2021 <span>Abstract submission deadline</span></p> 
-                            {/* <div className="box"></div> */}
                         </div>
-
-                        {/* <div className="timeline-text">
-                        <span className="text"> </span>
-                        </div> */}
                     </div>
+                    
                     <div className="card">
                         <div className="timeline-date">
                         <p className="date" > 30th October 2021 <span>Notification of acceptance/rejection</span> </p>
                         </div>
-
-                        {/* <div className="timeline-text">
-                        <p className="text"> Notification of acceptance/rejection</p>
-                        </div> */}
                     </div>
 
                     <div className="card">
                         <div className="timeline-date">
                         <p className="date" > 15th November 2021 <span>Final paper submission deadline</span> </p>
                         </div>
-
-                        {/* <div className="timeline-text">
-                        <p className="text"> Final paper submission deadline</p>
-                        </div> */}
                     </div>
 
                     <div className="card">
                         <div className="timeline-date">
                         <p className="date" > 30th October 2021 <span>Abstract submission deadline</span> </p>
-                        </div>
-
-                        {/* <div className="timeline-text">
-                        <p className="text"> Abstract submission deadline</p>
-                        </div> */}
+                        </div>  
                     </div>
                 </div>
 
