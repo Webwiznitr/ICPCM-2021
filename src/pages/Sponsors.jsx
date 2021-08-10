aa04b8f3f0462eda37c7a1b960dce3af723226f5
Ankur Kaushik <ankurkaushik2007@gmail.com>
2021-08-10 23:17:51 +0530

    fix:sponsors carousel added

--- src/pages/Sponsors.jsx
+++ src/pages/Sponsors.jsx
@@ -1,13 +1,44 @@
 import React from 'react'
-
+import Slider from 'infinite-react-carousel'
+import styled from "styled-components"; 
+import SponsorImage from "./sponsors.jpg"; 
 //Carousal of confirmed sponsors
+
+const Item = styled.div`
+width: 80vw; 
+margin: 10px auto; 
+}`;
+
+const SponsorImageStyle = {
+  padding: "10px", 
+  margin: "10px",
+}
+
 
 const Sponsors = () => {
     return (
-        <>
-            Sponsors
-        </>
+      <Item className = "sponsors-carousel" >
+      <Slider shift = { 10} className = "carousel-initialised" style = { { margin: "10px", padding: " 10px"}} autoplay = {true}  slidesToShow = {3} arrows = { false} dots>
+      
+      <img src= {SponsorImage} alt= " Image not found" style = { SponsorImageStyle} />
+      <img src= {SponsorImage} alt= " Image not found" style = { SponsorImageStyle} />
+      <img src= {SponsorImage} alt= " Image not found" style = { SponsorImageStyle} />
+      <img src= {SponsorImage} alt= " Image not found" style = { SponsorImageStyle} />
+      <img src= {SponsorImage} alt= " Image not found" style = { SponsorImageStyle} />
+      <img src= {SponsorImage} alt= " Image not found" style = { SponsorImageStyle} />
+      <img src= {SponsorImage} alt= " Image not found" style = { SponsorImageStyle} />
+      <img src= {SponsorImage} alt= " Image not found" style = { SponsorImageStyle} />
+
+
+    </Slider>
+    </Item>
+
     );
 }
 
 export default Sponsors;
+
+
+
+
+
