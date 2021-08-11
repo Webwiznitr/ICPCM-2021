71319bacaccb88a88cae45c2bd1947e6cd2e77e1
Ankur Kaushik <ankurkaushik2007@gmail.com>
2021-08-11 19:50:44 +0530

    fix:netifly error

--- src/pages/Sponsors.jsx
+++ src/pages/Sponsors.jsx
@@ -18,16 +18,16 @@ const SponsorImageStyle = {
 const Sponsors = () => {
     return (
       <Item className = "sponsors-carousel" >
-      <Slider shift = { 10} className = "carousel-initialised" style = { { margin: "10px", padding: " 10px"}} autoplay = {true}  slidesToShow = {3} arrows = { false} dots>
+      <Slider  className = "carousel-initialised" style = { { margin: "10px", padding: " 10px"}} autoplay = {true}  slidesToShow = {3} arrows = { false} dots>
       
-      <img src= {SponsorImage} alt= " Image not found" style = { SponsorImageStyle} />
-      <img src= {SponsorImage} alt= " Image not found" style = { SponsorImageStyle} />
-      <img src= {SponsorImage} alt= " Image not found" style = { SponsorImageStyle} />
-      <img src= {SponsorImage} alt= " Image not found" style = { SponsorImageStyle} />
-      <img src= {SponsorImage} alt= " Image not found" style = { SponsorImageStyle} />
-      <img src= {SponsorImage} alt= " Image not found" style = { SponsorImageStyle} />
-      <img src= {SponsorImage} alt= " Image not found" style = { SponsorImageStyle} />
-      <img src= {SponsorImage} alt= " Image not found" style = { SponsorImageStyle} />
+      <img src= {SponsorImage} alt= "not found" style = { SponsorImageStyle} />
+      <img src= {SponsorImage} alt= "not found" style = { SponsorImageStyle} />
+      <img src= {SponsorImage} alt= "not found" style = { SponsorImageStyle} />
+      <img src= {SponsorImage} alt= "not found" style = { SponsorImageStyle} />
+      <img src= {SponsorImage} alt= "not found" style = { SponsorImageStyle} />
+      <img src= {SponsorImage} alt= "not found" style = { SponsorImageStyle} />
+      <img src= {SponsorImage} alt= "not found" style = { SponsorImageStyle} />
+      <img src= {SponsorImage} alt= "not found" style = { SponsorImageStyle} />
 
 
     </Slider>
