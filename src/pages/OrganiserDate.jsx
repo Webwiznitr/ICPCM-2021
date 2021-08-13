19a185c44555b9a8be70bc7f50a419a2b271d2b8
a-sambhab <saisambhav21chaini@gmail.com>
2021-08-13 18:58:25 +0530

    Added organiser data

--- src/pages/OrganiserDate.jsx
+++ src/pages/OrganiserDate.jsx
@@ -1,22 +1,22 @@
 const Orgdata = [
     {
         name:"Prof. Anindya Basu",
-        imgsrc:"",
+        imgsrc:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1628853985/redsea/Anindya_basu_u6yxds.png",
         pos:"Chairman"
     },
     {
         name:"Prof. Archana Mallik",
-        imgsrc:"",
+        imgsrc:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1628853992/redsea/Archana_Mallick_qsyxhh.png",
         pos:"Convener"
     },
     {
         name:"Prof. Kumud Kant Mehta",
-        imgsrc:"",
+        imgsrc:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1628853984/redsea/mehta_ks5x3q.png",
         pos:"Co-convener"
     },
     {
         name:"Prof. Krishna Dutta",
-        imgsrc:"",
+        imgsrc:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1628853988/redsea/Krishna_Dutta_yejhrq.png",
         pos:"Treasurer"
     },
 ];
