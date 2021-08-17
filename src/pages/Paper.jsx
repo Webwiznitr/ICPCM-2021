e2b1ae47341e98b3ec82c70e1300bcffcf5601f4
manjeetdhayal <manjeetdhayal786@gmail.com>
2021-08-17 12:16:22 +0530

    fixes

--- src/pages/Paper.jsx
+++ src/pages/Paper.jsx
@@ -9,40 +9,41 @@ background-color: ${Colors.classic}
 color: ${Colors.light} ; 
 font-size: 1.5rem; 
 font-weight: bold; 
-padding: 10vw; 
+padding: 2vw 10vw; 
 text-align: left; 
 `
 ; 
 
 const ImageContainerPublication = styled.div`
  width: 100vw; 
- padding: 10vw; 
+ padding: 0 10vw; 
  display: flex; 
+ flex-wrap: wrap;
  justify-content: center; 
  align-items: center; 
 `; 
 
 const PrevImages = [
     {
-        images:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1629180658/redsea/NCPCM_2011_wiihwo.tiff"
+        images:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1629180658/redsea/NCPCM_2011_wiihwo.png"
     },
     {
-        images:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1629180652/redsea/NCPCM_2012_enrtd8.tiff"
+        images:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1629180652/redsea/NCPCM_2012_enrtd8.png"
     },
     {
-        images:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1629180659/redsea/NCPCM_2013_lxi2ld.tiff"
+        images:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1629180659/redsea/NCPCM_2013_lxi2ld.png"
     },
     {
-        images:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1629180651/redsea/NCPCM_2014_gate7a.tiff"
+        images:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1629180651/redsea/NCPCM_2014_gate7a.png"
     },
     {
-        images:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1629180652/redsea/NCPCM_2015_u4jial.tiff"
+        images:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1629180652/redsea/NCPCM_2015_u4jial.png"
     },
     {
-        images:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1629180649/redsea/NCPCM_2016_mouhl1.tiff"
+        images:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1629180649/redsea/NCPCM_2016_mouhl1.png"
     },
     {
-        images:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1629180651/redsea/NCPCM_2017_dqylem.tiff"
+        images:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1629180651/redsea/NCPCM_2017_dqylem.png"
     },
 
 ]
@@ -59,7 +60,9 @@ You are invited to submit abstracts and contribute to the conference. ISBN publi
 <ImageContainerPublication>
     {
         PrevImages.map(element => {
-            return <img src = { element.images} alt= "not found"/>
+            return <img style = { { 
+                margin: "10px", 
+            }} src = { element.images} alt= "not found"/>
         })
     }
 
