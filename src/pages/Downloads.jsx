b823fe48b79584094789066737458ce6ea9c6dc3
manjeetdhayal <manjeetdhayal786@gmail.com>
2021-08-18 19:49:36 +0530

    downloads fixed

--- src/pages/Downloads.jsx
+++ src/pages/Downloads.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import BannerSmall from "../components/BannerSmall";
-import "./Download.css";
 import {GoDesktopDownload} from 'react-icons/go'
 import {FaFileDownload} from 'react-icons/fa'
 import {IoMdDownload} from 'react-icons/io'
+import { Link } from "react-router-dom";
 
 const deskDown = <GoDesktopDownload size= "40px" />
 const flyDown  = <FaFileDownload size = "40px"/>
@@ -13,7 +13,7 @@ const Down = [
    {
      logo: deskDown, 
      name: "ICPCM 21 Abstract template",
-     downloadLink:"https://res.cloudinary.com/manjeetdhayal/image/upload/v1629233646/redsea/Downloads/Abstract_template_ibdm5b.png"
+     downloadLink:"/files/abstractTemplate.docx"
    }
    ,
    {
@@ -41,7 +41,7 @@ const Download = () => {
             <div className = "download_logo">{element.logo}</div>
             <div className = "download_info">
             <snap> {element.name}</snap>
-            <a href={element.downloadLink} target = "_blank" download >Download</a>
+            <Link to={element.downloadLink} target = "_blank" download >Download</Link>
             </div>
             </div>
             </div>
